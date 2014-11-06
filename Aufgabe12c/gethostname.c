c69b43390c454e0d24b18af1bba91e6d57cd3d0d
daniel <daniel.schroeder7@yahoo.de>
2014-11-06 23:18:25 +0100

    Auflösungsprozess der IP-Adresse vereinfacht

--- Aufgabe12c/gethostname.c
+++ Aufgabe12c/gethostname.c
@@ -7,33 +7,33 @@
 #include <netdb.h>
 
 int main(int argc, char **argv){
-	int error;
-	struct addrinfo *res, *resorig, hints;
-	char host[NI_MAXHOST], numhost[NI_MAXHOST];
-
 	if (argc != 2)
-		errx(1, "usage: %s <ipaddress>", basename(argv[0]));
+		errx(1, "usage: %s <ipaddress>", argv[0]);
+
+	struct addrinfo *res, *resorig, hints;
+	char host[NI_MAXHOST];
+	int err;
 
 	memset(&hints, 0, sizeof (hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_family = AF_INET;
 
 	/*
 	 * Hole sockaddr struct mit getaddrinfo(). Da Portname egal - servname = NULL
 	*/
-	if ((error = getaddrinfo(argv[1], NULL, &hints, &res)) != 0)
-		errx(1, "%s", gai_strerror(error));
+	if ((err = getaddrinfo(argv[1], NULL, &hints, &res)) != 0)
+		errx(1, "%s", gai_strerror(err));
 
 	for (resorig = res; res != NULL; res = res->ai_next) {
 		/* getnameinfo() anstatt inet_ntop() zum umwandeln der Adresse */
-		if ((error = getnameinfo(res->ai_addr, res->ai_addrlen,
+		if ((err = getnameinfo(res->ai_addr, res->ai_addrlen,
 		    numhost, sizeof (numhost), NULL, 0, NI_NUMERICHOST)) != 0)
 			errx(1, "%s", gai_strerror(error));
 		//Adresse auflösen
 		if ((error = getnameinfo(res->ai_addr, res->ai_addrlen,
 		    host, sizeof (host), NULL, 0, 0)) != 0)
-			errx(1, "%s", gai_strerror(error));
-		printf("IP-Adresse %s wird zu %s aufgelöst\n", numhost, host);
+			errx(1, "%s", gai_strerror(err));
+		printf("IP-Adresse %s wird zu %s aufgelöst\n", argv[1], host);
 	}
 
 	freeaddrinfo(resorig);
